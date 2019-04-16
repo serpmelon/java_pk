3fe692f2c51ae63402e8ce16ddb083a7a77a0889
serpmelon <tyn243222791@126.com>
2019-04-16 20:46:16 +0800

    Socket用到的常量，比如端口等

---
+++ src/main/java/com/togo/java/io/constant/SocketConstant.java
@@ -0,0 +1,8 @@
+package com.togo.java.io.constant;
+
+public class SocketConstant {
+
+	public static final int PORT = 5000;
+	public static final String HOST = "localhost";
+	public static final String UTF8 = "UTF-8";
+}
