08add3cc8a8a5a13cbb511f4a0b91efef4a2c940
serpmelon <tyn243222791@126.com>
2019-06-05 14:03:19 +0800

    将生成数组方法移动到ArrayUtil中

--- src/main/java/com/togo/java/data/algorithm/sort/TestSort.java
+++ src/main/java/com/togo/java/data/algorithm/sort/TestSort.java
@@ -1,15 +1,17 @@
 package com.togo.java.data.algorithm.sort;
 
+import com.togo.java.data.algorithm.util.ArrayUtil;
+
 import java.util.Random;
 
 public class TestSort {
 
-	private static Random random = new Random();
+
 
 	public static void main(String[] args) {
 
 		int n = 7;
-		int[] a = createArray(n, 0, 1000);
+		int[] a = ArrayUtil.createArray(n, 0, 1000);
 
 		AbstractSort sort = new BubbleSort();
 		sort.sortAndPrint(a.clone());
@@ -30,41 +32,5 @@ public class TestSort {
 //		sort.sortAndPrint(a.clone());
 	}
 
-	/**
-	 * 
-	 * <p>
-	 * Method ：createArray
-	 * <p>
-	 * Description :
-	 *
-	 * @param n
-	 *            数组长度
-	 * @param min
-	 *            数组中最小值
-	 * @param max
-	 *            数组中最大值
-	 * @return
-	 * @author taiyn
-	 *         <p>
-	 *         --------------------------------------------------------------<br>
-	 *         修改履历：<br>
-	 *         <li>2019年3月31日，taiyn，创建方法；<br>
-	 *         --------------------------------------------------------------<br>
-	 *         </p>
-	 */
-	public static int[] createArray(int n, int min, int max) {
 
-		int[] a = new int[n];
-		if (n <= 10)
-			System.out.println("create array :");
-		for (int i = 0; i < n; i++) {
-
-			a[i] = random.nextInt(max - min) + min;
-			if (n <= 10)
-				System.out.print(a[i] + " ");
-		}
-
-		System.out.println();
-		return a;
-	}
 }
--- src/main/java/com/togo/java/data/algorithm/util/ArrayUtil.java
+++ src/main/java/com/togo/java/data/algorithm/util/ArrayUtil.java
@@ -1,6 +1,10 @@
 package com.togo.java.data.algorithm.util;
 
+import java.util.Random;
+
 public class ArrayUtil {
+
+	private static Random random = new Random();
 
 	private ArrayUtil() {
 	}
@@ -53,5 +57,43 @@ public class ArrayUtil {
 
 			des[i] = (int) src[i];
 		}
+	}
+
+	/**
+	 *
+	 * <p>
+	 * Method ：createArray
+	 * <p>
+	 * Description :
+	 *
+	 * @param n
+	 *            数组长度
+	 * @param min
+	 *            数组中最小值
+	 * @param max
+	 *            数组中最大值
+	 * @return
+	 * @author taiyn
+	 *         <p>
+	 *         --------------------------------------------------------------<br>
+	 *         修改履历：<br>
+	 *         <li>2019年3月31日，taiyn，创建方法；<br>
+	 *         --------------------------------------------------------------<br>
+	 *         </p>
+	 */
+	public static int[] createArray(int n, int min, int max) {
+
+		int[] a = new int[n];
+		if (n <= 10)
+			System.out.println("create array :");
+		for (int i = 0; i < n; i++) {
+
+			a[i] = random.nextInt(max - min) + min;
+			if (n <= 10)
+				System.out.print(a[i] + " ");
+		}
+
+		System.out.println();
+		return a;
 	}
 }
