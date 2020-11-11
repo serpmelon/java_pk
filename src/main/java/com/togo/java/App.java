243175fac130b7b8601568f68c1a1d0f020a0f59
serpmelon <tyn243222791@126.com>
2020-11-11 09:54:36 +0800

    feature: 增加图的代码

--- src/main/java/com/togo/java/App.java
+++ src/main/java/com/togo/java/App.java
@@ -1,14 +1,79 @@
 package com.togo.java;
 
-import java.util.HashMap;
+import com.togo.java.bytecode.ClassBuilder;
+import com.togo.java.bytecode.HelloByte;
+import com.togo.java.bytecode.load.Wa;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+import java.io.File;
+import java.lang.reflect.Method;
 
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
 
-        HashMap map = new HashMap();
+        ClassBuilder cb = new ClassBuilder();
+//
+//
+//        // 生成二进制字节码
+//        byte[] bytes = generate();
+//        // 使用自定义的ClassLoader
+        HelloByte.MyClassLoader cl = new HelloByte.MyClassLoader();
+        // 加载我们生成的 HelloWorld 类
+        Class<?> clazz = cl.defineClass("com.togo.java.bytecode.load.Peo", Wa.dump());
+        // 反射获取 main 方法
+        Method method = clazz.getMethod("print", null);
+        Object a = clazz.newInstance();
+        Object obj = method.invoke(a, null);
+        System.out.println(obj);
+
+        System.out.println(System.getProperty("user.dir"));
+        File directory = new File("");//设定为当前文件夹
+        try{
+            System.out.println(directory.getCanonicalPath());//获取标准的路径
+            System.out.println(directory.getAbsolutePath());//获取绝对路径
+        }catch(Exception e){}
+
+        System.out.println(App.class.getResource("").getPath());
+
+    }
+
+    private static byte[] generate() {
+
+        ClassWriter cw = new ClassWriter(0);
+        // 定义对象头：版本号、修饰符、全类名、签名、父类、实现的接口
+        cw.visit(Opcodes.V1_8, Opcodes.ACC_PUBLIC, "com/dadiyang/asm/HelloWorld",
+                null, "java/lang/Object", null);
+        // 添加方法：修饰符、方法名、描述符、签名、抛出的异常
+        MethodVisitor mv = cw.visitMethod(Opcodes.ACC_PUBLIC, "hi",
+                "([Ljava/lang/String;)V", null, null);
+        // 执行指令：获取静态属性
+        mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+        // 加载常量 load constant
+        mv.visitLdcInsn("HelloWorld!");
+        // 调用方法
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+        // 返回
+        mv.visitInsn(Opcodes.RETURN);
+        // 设置栈大小和局部变量表大小
+        mv.visitMaxs(2, 1);
+        // 方法结束
+        mv.visitEnd();
+        // 类完成
+        cw.visitEnd();
+        // 生成字节数组
+        return cw.toByteArray();
+    }
+
+    public static class MyClassLoader extends ClassLoader {
+        public Class<?> defineClass(String name, byte[] b) {
+            // ClassLoader是个抽象类，而ClassLoader.defineClass 方法是protected的
+            // 所以我们需要定义一个子类将这个方法暴露出来
+            return super.defineClass(name, b, 0, b.length);
+        }
     }
 }
---
+++ src/main/java/com/togo/java/data/structure/graph/Graph.java
@@ -0,0 +1,34 @@
+package com.togo.java.data.structure.graph;
+
+/**
+ * @Author taiyn
+ * @Description TODO
+ * @Date 9:23 上午 2020/11/11
+ **/
+public class Graph {
+
+    /**
+     * 定点数量
+     */
+    private int v;
+    /**
+     * 变的数量
+     */
+    private int w;
+
+
+    public Graph(int v) {
+
+        this.v = v;
+    }
+
+
+
+    public int getV() {
+        return v;
+    }
+
+    public int getW() {
+        return w;
+    }
+}
